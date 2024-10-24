f4c0677bd0e6976b501908bbacb581e94f2fa15d
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-10-24 11:18:58 -0600

    Add musepack-tools

---
+++ packages/musepack-tools/build.sh
@@ -0,0 +1,25 @@
+# http(s) link to package home page.
+TERMUX_PKG_HOMEPAGE=https://www.musepack.net
+
+# One-line, short package description.
+TERMUX_PKG_DESCRIPTION="MusePack commandline utilities"
+
+# License.
+# Use SPDX identifier: https://spdx.org/licenses/
+TERMUX_PKG_LICENSE="BSD/GNU LGPL"
+
+# Who cares about package.
+# Specify yourself (Github nick, or name + email) if you wish to maintain the
+# package, fix its bugs, etc. Otherwise specify "@termux".
+# Please note that unofficial repositories are not allowed to reference @termux
+# as their maintainer.
+TERMUX_PKG_MAINTAINER="@ego-lay-atman-bay"
+
+# Version.
+TERMUX_PKG_VERSION=r475
+
+# URL to archive with source code.
+TERMUX_PKG_SRCURL=https://files.musepack.net/source/musepack_src_${TERMUX_PKG_VERSION}.tar.gz
+
+# SHA-256 checksum of the source code archive.
+TERMUX_PKG_SHA256=a4b1742f997f83e1056142d556a8c20845ba764b70365ff9ccf2e3f81c427b2b
